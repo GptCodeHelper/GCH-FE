2fda3ef09e6842cd02c5817eca07859d9b26c895
waitzero <magic2eye3@gmail.com>
2025-01-04 16:54:10 +0900

    feat:디테일링

--- app/exam/exam.tsx
+++ app/exam/exam.tsx
@@ -1,5 +1,6 @@
 import React, { MouseEvent, useState, useEffect } from "react";
 import styles from "./exam.Layout.module.css";
+import '../global/GNB'
 
 const View: React.FC = () => {
   const [isResizing, setIsResizing] = useState(false);
@@ -54,72 +55,75 @@ const View: React.FC = () => {
   }, [isResizing]);
 
   return (
-    <div className={styles.examMain}>
-      {/* Secondary Navigation Bar */}
-      <div className={styles.secnb}>
-        <div className={styles.pageLeft}>
-          <button>북마크</button>
-          <a href="#">제목</a>
-        </div>
-        <div className={styles.pageRight}>
-          <div>
-            <button>테스크 케이스 추가</button>
-            <select className={styles.rightchildren}>
-              <option value="sublime">Sublime</option>
-              <option value="vim">Vim</option>
-              <option value="emacs">Emacs</option>
-            </select>
-            <select>
-              <option value="java">Java</option>
-              <option value="javascript">JavaScript</option>
-              <option value="python">Python</option>
-            </select>
+      <div className={styles.examMain}>
+        {/* Secondary Navigation Bar */}
+        <div className={styles.secnb}>
+          <div className={styles.pageLeft}>
+            <button>북마크</button>
+            <a href="#">제목</a>
           </div>
-        </div>
-      </div>
-      {/* Main Content Area */}
-      <div className={styles.mainContent}>
-        {/* Question Box (Left) */}
-        <div
-          className={styles.questLimit}
-          onMouseDown={handleMouseDown}
-          style={{ width: `${width}px` }}
-        >
-          <div className={styles.questBox}>
-          문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 문제가 나옵니다 
-            {/* Content */}
-          </div>
-        </div>
-        {/* Answer&Terminer Input Box */}
-        <div className={styles.answerBox}>
-          {/*Input bax*/}
-          <div className={styles.inputBox}>
-            <form>
-              <input placeholder="문제 입력 form" />
-            </form>
-          </div>
-          {/*Terminer*/}
-            <div className={styles.terminerBox}>
-              <div className={styles.terminerSize}
-              onMouseDown={handleResize}>=</div>
-                <div className={styles.terminerLender}>
-                  터미널 렌더링
-                </div>
-            </div>
-          <div>
+          <div className={styles.pageRight}>
+            <div>
+              {/*모달창*/}
+              <button id="Tcmodal">테스크 케이스 추가</button>
 
+
+              <select className={styles.rightchildren}>
+                <option value="sublime">Sublime</option>
+                <option value="vim">Vim</option>
+                <option value="emacs">Emacs</option>
+              </select>
+              <select>
+                <option value="java">Java</option>
+                <option value="javascript">JavaScript</option>
+                <option value="python">Python</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        {/* Main Content Area */}
+        <div className={styles.mainContent}>
+          {/* Question Box (Left) */}
+          <div
+            className={styles.questLimit}
+            onMouseDown={handleMouseDown}
+            style={{ width: `${width}px` }}
+          >
+            <div className={styles.questBox}>
+            code mirror 사용 예정
+              {/* Content */}
+            </div>
+          </div>
+          {/* Answer&Terminer Input Box */}
+          <div className={styles.answerBox}>
+            {/*Input bax*/}
+            <div className={styles.inputBox}>
+              <form>
+                <input placeholder="문제 입력 form" />
+              </form>
+            </div>
+            {/*Terminer*/}
+              <div className={styles.terminerBox}>
+                <div className={styles.terminerSize}
+                onMouseDown={handleResize}>=</div>
+                  <div className={styles.terminerLender}>
+                    터미널 렌더링
+                  </div>
+              </div>
+            <div>
+
+            </div>
+          </div>
+        </div>
+        {/* Bottom Navigation Bar */}
+        <div className={styles.bottomNav}>
+          <div className={styles.bottomNavchildren}>
+            <button>저장</button>
+            <button>테스트</button>
+            <button>실행</button>
           </div>
         </div>
       </div>
-      {/* Bottom Navigation Bar */}
-      <div className={styles.bottomNav}>
-        <div className={styles.bottomNavchildren}>
-          <button>저장</button>
-          <button>테스트</button>
-          <button>실행</button>
-        </div>
-      </div>
-    </div>
   );
 };
 
