d447d5c4b56beda7bfb0e2225bfe920f51e131dc
Jaesoooooooone <91954574+Jae-soon@users.noreply.github.com>
2025-03-17 22:19:22 +0900

    feat : successYn이 N일 경우 Error Code Alert 설정

--- utils/axiosInstance.tsx
+++ utils/axiosInstance.tsx
@@ -5,4 +5,20 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+// 응답 인터셉터 추가
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // 응답 데이터가 ResponseData 형식이라고 가정
+    if (response.data && response.data.successYn === "N") {
+      alert(response.data.code);
+      // 실패 상황으로 간주하고 Promise.reject()를 호출하여 후속 처리를 catch로 넘김
+      return Promise.reject(new Error(response.data.code));
+    }
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
